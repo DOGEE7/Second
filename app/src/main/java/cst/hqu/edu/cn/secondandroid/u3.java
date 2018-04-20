9e9c5c300097e29785be43712a5bdac00affab56
JINYANI <1067826433@qq.com>
2018-04-20 21:26:00 +0800

    2

--- app/src/main/java/cst/hqu/edu/cn/secondandroid/u3.java
+++ app/src/main/java/cst/hqu/edu/cn/secondandroid/u3.java
@@ -5,13 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class u3 extends AppCompatActivity {
+    int []images = new int[]{
+            R.drawable.ele,
+            R.drawable.fox,
+            R.drawable.gf,
+            R.drawable.mon,
+            R.drawable.pig
+    };
+    int currentImage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.u3);
+        final ImageView image = new ImageView(this);
+        FrameLayout main = (FrameLayout)findViewById(R.id.main);
+        main.addView(image);
+        image.setImageResource(images[++currentImage % images.length]);
+
 
         Button bnreturn = (Button) findViewById(R.id.bnreturn);
         bnreturn.setOnClickListener(new View.OnClickListener() {
